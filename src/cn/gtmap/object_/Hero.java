d68ad0f27351eb765f5602257fba0b112413267f
TWM <15152806231@163.com>
2020-01-09 12:43:48 +0800

    Object类是所有类的父类

---
+++ src/cn/gtmap/object_/Hero.java
@@ -0,0 +1,18 @@
+package cn.gtmap.object_;
+
+/**
+ * Object类是所有类的父类
+ *	声明一个类的时候，默认是继承了Object 
+ */
+public class Hero extends Object{
+    String name; //姓名
+    
+    float hp; //血量
+        
+    float armor; //护甲
+        
+    int moveSpeed; //移动速度
+    
+    
+    
+}
